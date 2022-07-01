305040fb06efbe55c47fcea382a829b0ef80b265
NickMendel <nick.mendel@student.htw-berlin.de>
2022-07-01 23:23:13 +0200

    fix

--- src/main/java/de/htw/webtechdemo/section/SectionEntity.java
+++ src/main/java/de/htw/webtechdemo/section/SectionEntity.java
@@ -9,116 +9,7 @@ public class SectionEntity {
     @ElementCollection
     List<String> sections = new ArrayList<>();
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
 
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<Section> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean add(Section section) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends Section> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int index, Collection<? extends Section> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public Section get(int index) {
-            return null;
-        }
-
-        @Override
-        public Section set(int index, Section element) {
-            return null;
-        }
-
-        @Override
-        public void add(int index, Section element) {
-
-        }
-
-        @Override
-        public Section remove(int index) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public ListIterator<Section> listIterator() {
-            return null;
-        }
-
-        @Override
-        public ListIterator<Section> listIterator(int index) {
-            return null;
-        }
-
-        @Override
-        public List<Section> subList(int fromIndex, int toIndex) {
-            return null;
-        }
-    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -163,5 +54,116 @@ public class SectionEntity {
         this.description = description;
     }
 
-}
+        public boolean isEmpty() {
+            return false;
+        }
+
+
+        public boolean contains(Object o) {
+            return false;
+        }
+
+
+        public Iterator<Section> iterator() {
+            return null;
+        }
+
+
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+
+        public boolean add(Section section) {
+            return false;
+        }
+
+
+        public boolean remove(Object o) {
+            return false;
+        }
+
+
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+
+        public boolean addAll(Collection<? extends Section> c) {
+            return false;
+        }
+
+
+        public boolean addAll(int index, Collection<? extends Section> c) {
+            return false;
+        }
+
+
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+
+        public void clear() {
+
+        }
+
+
+        public Section get(int index) {
+            return null;
+        }
+
+
+        public Section set(int index, Section element) {
+            return null;
+        }
+
+
+        public void add(int index, Section element) {
+
+        }
+
+
+        public Section remove(int index) {
+            return null;
+        }
+
+
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+
+        public ListIterator<Section> listIterator() {
+            return null;
+        }
+
+
+        public ListIterator<Section> listIterator(int index) {
+            return null;
+        }
+
+
+        public List<Section> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    }
+
+
+
 
